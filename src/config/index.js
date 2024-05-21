20d69826d11e65aec98566174045f2458b2bb042
dngiang <dongocgiang.edu@gmail.com>
2024-05-22 02:00:07 +0700

    ✨ Update config env

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 
 const env = {
   port: +process.env.PORT || 3000,
+  urlUpload: process.env.URL_UPLOAD,
+  tiktokCookie: process.env.TIKTOK_COOKIE,
+  maxFileSize: +process.env.MAX_FILE_SIZE || 5,
 };
 
 module.exports = env;
